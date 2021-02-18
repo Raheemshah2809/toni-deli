de75c1689fa9139982acb839fa8d7b43d3d4aef3
Danny Castillo <dannyhgo@hotmail.com>
2021-02-18 22:33:15 +0100

    Add Carousel slider

--- js/script.js
+++ js/script.js
@@ -21,119 +21,62 @@ headerObserver.observe(header);
 
 /***************************************
  * 
- * Review Functionality
+ * Review Carousel
  * 
  **************************************/
-HTMLElement.prototype.forEachElement = function(elementType, callback) {
-    if(elementType && callback) {
-        let elements = this.querySelectorAll(elementType);
-        Array.from(elements).forEach((element, index) => {
-            callback(element, index);
-        });
-    }
+// Variables //
+const slideContainer = document.querySelector('.slide-container');
+const slide = document.querySelector('.slides');
+let slides = document.querySelectorAll('.slide');
+
+// Carousel Affects
+let index = 1;
+const interval = 5000;
+let slideId;
+
+// Clones for infitine loop //
+const firstClone = slides[0].cloneNode(true);
+const lastClone = slides[slides.length - 1].cloneNode(true);
+
+// ID for clones
+firstClone.id = 'first-clone';
+lastClone.id = 'last-clone';
+
+slide.append(firstClone);
+slide.prepend(lastClone);
+
+// Find the width of the element
+let slideWith = slides[index].clientWidth;
+
+// Make sure the first image is overlapping the cloned one
+slide.style.transform = `translateX(${-slideWith * index}px)`;
+
+// Automatic Carousel
+const startSlide = () => {
+    slideId = setInterval(() => {
+        index++;
+        slide.style.transform = `translateX(${-slideWith * index}px)`;
+        slide.style.transition = '2s ease-in-out';
+    }, interval);
 };
 
-HTMLElement.prototype.addClass = function(className) {
-    if(className) {
-        this.classList.add(className);
+// Repeat loop once the image have come to the end
+slide.addEventListener('transitionend', () => {
+    let slides = document.querySelectorAll('.slide');
+    if(slides[index].id === firstClone.id) {
+        slide.style.transition = 'none';
+        index = 1;
+        slide.style.transform = `translateX(${-slideWith * index}px)`;
     }
-};
-
-HTMLElement.prototype.containsClass = function(className) {
-    if(className) {
-        return this.classList.contains(className);
-    }
-    return false;
-};
-
-HTMLElement.prototype.removeClass = function(className) {
-    if (className) {
-        if (this.containsClass(className)) {
-            this.classList.remove(className);
-        }
-    }
-};
-
-let TestimonialSlider = (function () {
-    class TestimonialSlider {
-        constructor(slider) {
-            let _this = this;
-            this.maxSlides = 0;
-            this.currentSlideNumber = 0;
-            this.slides = [];
-            this.displayTime = 5000;
-            const errorMessage = 'please provide a valid testimonialSlider';
-
-            /** checking that a valid testimoanl-slider was provided. */
-            if (!slider) {
-                throw TypeError(errorMessage);
-            } else {
-                /** testimonial slider must contain the class testimonial-slider */
-                if (!slider.containsClass('testimonial-slider')) {
-                    throw TypeError(errorMessage);
-                }
-            }
-            /** appending the slides to the array and making sure the first one is visible */
-            slider.forEachElement('li', (slide, index) => {
-                if (index == 0) {
-                    slide.addClass('selected');
-                }
-                this.addSlide(slide);
-            });
-            setInterval(function () {
-                //         // if the currentSlide Number is greater than the maxSlides, set it to 0
-                //         if(_this.currentSlideNumber > _this.maxSlides) {
-                //           _this.currentSlideNumber = 0;
-                //           // removing the last slide from the screen
-                //           _this.getSlide(maxSlides).removeClass('selected');
-                //         } 
-
-                //         // making the current slide visible
-                //         // _this.previousSlide().removeClass('selected');
-                //         _this.nextSlide().addClass('selected');
-                //         _this.currentSlideNumber++;
-
-                // if the current slide number is greater than or equal to the max slides, reset the slide number to 0 and remove the last element from the screen.
-                let hideSlideIndex = 0;
-                if (_this.currentSlideNumber >= _this.maxSlides) {
-                    _this.currentSlideNumber = 0;
-                    hideSlideIndex = _this.maxSlides - 1;
-                } else {
-                    hideSlideIndex = _this.currentSlideNumber > 0 ? _this.currentSlideNumber - 1 : 0;
-                }
-
-                // removing previous slide from screen 
-                _this.getSlide(hideSlideIndex).removeClass('selected');
-
-                // adding new slide to screen.
-                let currentSlide = _this.getSlide(_this.currentSlideNumber);
-                currentSlide.addClass('selected');
-
-                _this.currentSlideNumber++;
-            }, this.displayTime);
-        }
-
-        getSlide(index) {
-            if (index < this.maxSlides) {
-                return this.slides[index];
-            }
-                
-        }
-
-        addSlide(slide) {
-            this.slides.push(slide);
-            this.maxSlides++;
-        }
-    }
-
-    return TestimonialSlider;
-})();
-
-
-let testimonials = [];
-/** getting all the testimonal-sliders */
-const testimonialSlider = Array.from(document.querySelectorAll('.testimonial-slider'));
-
-testimonialSlider.forEach(slider => {
-    testimonials.push(new TestimonialSlider(slider));
 });
+
+// Stop the slider on mouseover
+slideContainer.addEventListener('mouseenter', () => {
+    clearInterval(slideId);
+});
+
+// continue Slider on mouse exit
+slideContainer.addEventListener('mouseleave', startSlide);
+
+
+startSlide();
